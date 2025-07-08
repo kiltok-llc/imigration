4d56f48b66d4954e48950b2d50cef39d6c26a640
Matias Kotlik <matiaskotlik@users.noreply.github.com>
2025-07-08 14:00:22 -0400

    fix: build

--- apps/mobile/src/app/documents/index.tsx
+++ apps/mobile/src/app/documents/index.tsx
@@ -1,7 +1,5 @@
 import * as Sentry from '@sentry/react-native';
-import { useMutation } from '@tanstack/react-query';
-import * as FileSystem from 'expo-file-system';
-import { Stack, useRouter } from 'expo-router';
+import { Stack } from 'expo-router';
 import { t } from 'i18next';
 import { Suspense } from 'react';
 import { View } from 'react-native';
@@ -11,7 +9,6 @@ import tw from 'twrnc';
 import { Trans } from '@/components/trans';
 import { Container } from '@/components/ui/container';
 import { useRNSuspenseQuery } from '@/hooks/use-rn-query';
-import { useTRPC } from '@/lib/trpc';
 import { documentsQueryOptions } from '@/queries/documents';
 
 // interface StoredDocument {
@@ -57,49 +54,11 @@ export default function DocumentsScreen() {
 function DocumentList() {
   const { data: documents } = useRNSuspenseQuery(documentsQueryOptions());
 
-  const router = useRouter();
-  const trpc = useTRPC();
-  const {
-    isPending,
-    mutate: handleGenerate,
-    variables,
-  } = useMutation(
-    trpc.document.generatePdf.mutationOptions({
-      meta: {
-        errorToast: 'Failed to generate PDF.',
-      },
-      async onSuccess({ data }) {
-        const fileUri = `${FileSystem.cacheDirectory}${randomId()}.pdf`;
-        await FileSystem.writeAsStringAsync(fileUri, data, {
-          encoding: 'base64',
-        });
-        router.push({
-          params: {
-            source: fileUri,
-          },
-          pathname: '/pdf-view',
-        });
-      },
-    })
-  );
-
   return (
     <Container style={tw`flex-1 gap-4`}>
       {documents && documents.length > 0 ? (
         documents.map((document) => (
-          <Button
-            disabled={isPending}
-            key={document.id}
-            loading={variables?.documentId === document.id && isPending}
-            mode='outlined'
-            onPress={() =>
-              handleGenerate({
-                documentId: document.id,
-                variables: {},
-              })
-            }
-            style={tw`w-full`}
-          >
+          <Button disabled key={document.id} mode='outlined' style={tw`w-full`}>
             {document.name}
           </Button>
         ))
@@ -110,13 +69,4 @@ function DocumentList() {
       )}
     </Container>
   );
-}
-
-function randomId(length = 12) {
-  let id = '';
-  const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz';
-  for (let i = 0; i < length; i++) {
-    id += chars[Math.floor(Math.random() * chars.length)];
-  }
-  return id;
 }
