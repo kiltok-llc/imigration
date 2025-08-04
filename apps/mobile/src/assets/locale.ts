9ea3026d41cd5471d33954c3398c90b7e36cad85
Matias Kotlik <matiaskotlik@users.noreply.github.com>
2025-08-03 19:37:33 -0700

    chore: test update

--- apps/mobile/src/assets/locale.ts
+++ apps/mobile/src/assets/locale.ts
@@ -163,7 +163,7 @@ export default {
         popular: 'Popular Services',
         screenTitle: 'Services',
         searchPlaceholder: 'Search services...',
-        title: 'iMigration Services TEST',
+        title: 'iMigration Services',
       },
       yes: 'Yes',
     },
