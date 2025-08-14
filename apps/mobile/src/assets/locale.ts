9c609066c32c1a4c89942f873930ef868c9e0f85
Matias Kotlik <matiaskotlik@users.noreply.github.com>
2025-08-14 16:01:51 -0500

    fix: update translations

--- apps/mobile/src/assets/locale.ts
+++ apps/mobile/src/assets/locale.ts
@@ -700,7 +700,7 @@ export default {
         popular: 'Popular Services',
         screenTitle: 'Services',
         searchPlaceholder: 'Search services...',
-        title: 'iMigration Services REBUILD',
+        title: 'iMigration Services',
       },
     },
   },
