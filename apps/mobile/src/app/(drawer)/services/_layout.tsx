68ab78ecd31375b1e83c52bf9a4f3bf948e1e520
Matias Kotlik <matiaskotlik@users.noreply.github.com>
2025-08-19 02:13:40 -0500

    fix: don't open drawer when swiping from quiz

--- apps/mobile/src/app/(drawer)/services/_layout.tsx
+++ apps/mobile/src/app/(drawer)/services/_layout.tsx
@@ -2,13 +2,17 @@ import Drawer from 'expo-router/drawer';
 import * as React from 'react';
 
 import { Stack } from '@/components/stack';
+import { useFocusedRouteName } from '@/hooks/use-route';
 
 export default function DrawerLayout() {
+  const routeName = useFocusedRouteName();
+
   return (
     <>
       <Drawer.Screen
         options={{
           headerShown: false,
+          swipeEnabled: routeName === 'index',
         }}
       />
       <Stack />
