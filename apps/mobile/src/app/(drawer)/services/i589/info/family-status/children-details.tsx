49015d2f9c0cb861158b38f45259ee618b680711
Matias Kotlik <matiaskotlik@users.noreply.github.com>
2025-09-04 02:13:38 -0400

    fix: quiz page ids

--- apps/mobile/src/app/(drawer)/services/i589/info/family-status/children-details.tsx
+++ apps/mobile/src/app/(drawer)/services/i589/info/family-status/children-details.tsx
@@ -14,7 +14,11 @@ import {
 import { FormBooleanInput, FormSexInput } from '@/components/form/radio';
 import { QuizDateInput } from '@/components/quiz/date';
 import { QuizFieldTitle, QuizPageTitle } from '@/components/quiz/label';
-import { QuizPage, QuizPageHandle } from '@/components/quiz/page';
+import {
+  QuizPage,
+  QuizPageHandle,
+  QuizPageProps,
+} from '@/components/quiz/page';
 import { QuizScreen } from '@/components/quiz/screen';
 import { QuizTextInput } from '@/components/quiz/text';
 import {
@@ -30,11 +34,6 @@ import { nameAtom } from '@/lib/data/user';
 import { SexEnum } from '@/lib/schemas';
 import { TranslationContextProvider } from '@/lib/translation';
 import { required, stretchTo } from '@/lib/utils';
-
-type ChildQuizPageProps = {
-  id: string;
-  index: number;
-};
 
 export default function ChildrenDetails() {
   const [childIds, setChildIds] = useAtom(childIdsAtom);
@@ -83,132 +82,133 @@ export default function ChildrenDetails() {
       </QuizPage>
 
       {childIds.map((id, index) => (
-        <ChildQuizPage id={id} index={index} key={id} />
+        <ChildQuizPage id={id} index={index} key={id} pageId='child' />
       ))}
     </QuizScreen>
   );
 }
 
-const ChildQuizPage = forwardRef<QuizPageHandle, ChildQuizPageProps>(
-  function ChildQuizPage({ id, index }, ref) {
-    const numberOfChildren = useAtomValue(childIdsAtom).length;
-    const setDob = useSetAtom(childDobAtom(id));
-    const setEthnicity = useSetAtom(childEthnicityAtom(id));
-    const setBirthCertificate = useSetAtom(childBirthCertificateAtom(id));
-    const setLivesInUsa = useSetAtom(childLivesInUsaAtom(id));
-    const setSex = useSetAtom(childSexAtom(id));
-    const setName = useSetAtom(childNameAtom(id));
-    const lastName = useAtomValue(nameAtom).last;
+const ChildQuizPage = forwardRef<
+  QuizPageHandle,
+  QuizPageProps & { id: string; index: number }
+>(function ChildQuizPage({ id, index, pageId }, ref) {
+  const numberOfChildren = useAtomValue(childIdsAtom).length;
+  const setDob = useSetAtom(childDobAtom(id));
+  const setEthnicity = useSetAtom(childEthnicityAtom(id));
+  const setBirthCertificate = useSetAtom(childBirthCertificateAtom(id));
+  const setLivesInUsa = useSetAtom(childLivesInUsaAtom(id));
+  const setSex = useSetAtom(childSexAtom(id));
+  const setName = useSetAtom(childNameAtom(id));
+  const lastName = useAtomValue(nameAtom).last;
 
-    return (
-      <QuizPage
-        defaultValues={{
-          dob: null,
-          ethnicity: '',
-          hasBirthCertificate: null,
-          livesInUsa: null,
-          name: {
-            ...DEFAULT_FORM_NAME,
-            last: lastName,
-          },
-          sex: null,
-        }}
-        onSuccess={({
-          birthCertificate,
-          dob,
-          ethnicity,
-          livesInUsa,
-          name,
-          sex,
-        }) => {
-          setDob(dob);
-          setEthnicity(ethnicity);
-          setBirthCertificate(birthCertificate ?? '');
-          setLivesInUsa(livesInUsa);
-          setName(name);
-          setSex(sex);
-        }}
-        pageId='child'
-        pageKey={id}
-        ref={ref}
-        schema={z.object({
-          birthCertificate: required(z.string().nullable()).optional(),
-          dob: required(z.date().nullable()),
-          ethnicity: z.string(),
-          hasBirthCertificate: required(z.boolean().nullable()),
-          livesInUsa: required(z.boolean().nullable()),
-          name: FormNameSchema,
-          sex: required(SexEnum.nullable()),
-        })}
-      >
-        {({ control, lens, watch }) => (
-          <TranslationContextProvider
-            value={{
-              context: watch('name.first') ? 'named' : 'unnamed',
-              count: index + 1,
-              values: {
-                name: watch('name.first'),
-                ordinal: true,
-                total: numberOfChildren,
-              },
-            }}
+  return (
+    <QuizPage
+      defaultValues={{
+        dob: null,
+        ethnicity: '',
+        hasBirthCertificate: null,
+        livesInUsa: null,
+        name: {
+          ...DEFAULT_FORM_NAME,
+          last: lastName,
+        },
+        sex: null,
+      }}
+      onSuccess={({
+        birthCertificate,
+        dob,
+        ethnicity,
+        livesInUsa,
+        name,
+        sex,
+      }) => {
+        setDob(dob);
+        setEthnicity(ethnicity);
+        setBirthCertificate(birthCertificate ?? '');
+        setLivesInUsa(livesInUsa);
+        setName(name);
+        setSex(sex);
+      }}
+      pageId={pageId}
+      pageKey={id}
+      ref={ref}
+      schema={z.object({
+        birthCertificate: required(z.string().nullable()).optional(),
+        dob: required(z.date().nullable()),
+        ethnicity: z.string(),
+        hasBirthCertificate: required(z.boolean().nullable()),
+        livesInUsa: required(z.boolean().nullable()),
+        name: FormNameSchema,
+        sex: required(SexEnum.nullable()),
+      })}
+    >
+      {({ control, lens, watch }) => (
+        <TranslationContextProvider
+          value={{
+            context: watch('name.first') ? 'named' : 'unnamed',
+            count: index + 1,
+            values: {
+              name: watch('name.first'),
+              ordinal: true,
+              total: numberOfChildren,
+            },
+          }}
+        >
+          <QuizPageTitle />
+
+          <FormBlock>
+            <QuizFieldTitle name='name' variant='titleLarge' />
+            <FormNameInput lens={lens.focus('name')} />
+          </FormBlock>
+
+          <FormBlock>
+            <FormField control={control} name='sex'>
+              <QuizFieldTitle variant='titleLarge' />
+              <FormSexInput />
+            </FormField>
+          </FormBlock>
+
+          <FormBlock>
+            <FormField control={control} name='dob'>
+              <QuizFieldTitle variant='titleLarge' />
+              <QuizDateInput />
+            </FormField>
+          </FormBlock>
+
+          <FormBlock>
+            <FormField control={control} name='livesInUsa'>
+              <QuizFieldTitle variant='titleLarge' />
+              <FormBooleanInput />
+            </FormField>
+          </FormBlock>
+
+          <FormBlock>
+            <FormField control={control} name='ethnicity'>
+              <QuizFieldTitle variant='titleLarge' />
+              <QuizTextInput optional />
+            </FormField>
+          </FormBlock>
+
+          <FormBlock>
+            <FormField control={control} name='hasBirthCertificate'>
+              <QuizFieldTitle variant='titleLarge' />
+              <FormBooleanInput />
+            </FormField>
+          </FormBlock>
+
+          <ConditionalFormWrapper
+            active={!!watch('hasBirthCertificate')}
+            activeValue={null}
+            control={control}
+            name='birthCertificate'
           >
-            <QuizPageTitle />
-
-            <FormBlock>
-              <QuizFieldTitle name='name' variant='titleLarge' />
-              <FormNameInput lens={lens.focus('name')} />
+            <FormBlock animated>
+              <QuizFieldTitle />
+              <FormImageInput />
             </FormBlock>
-
-            <FormBlock>
-              <FormField control={control} name='sex'>
-                <QuizFieldTitle variant='titleLarge' />
-                <FormSexInput />
-              </FormField>
-            </FormBlock>
-
-            <FormBlock>
-              <FormField control={control} name='dob'>
-                <QuizFieldTitle variant='titleLarge' />
-                <QuizDateInput />
-              </FormField>
-            </FormBlock>
-
-            <FormBlock>
-              <FormField control={control} name='livesInUsa'>
-                <QuizFieldTitle variant='titleLarge' />
-                <FormBooleanInput />
-              </FormField>
-            </FormBlock>
-
-            <FormBlock>
-              <FormField control={control} name='ethnicity'>
-                <QuizFieldTitle variant='titleLarge' />
-                <QuizTextInput optional />
-              </FormField>
-            </FormBlock>
-
-            <FormBlock>
-              <FormField control={control} name='hasBirthCertificate'>
-                <QuizFieldTitle variant='titleLarge' />
-                <FormBooleanInput />
-              </FormField>
-            </FormBlock>
-
-            <ConditionalFormWrapper
-              active={!!watch('hasBirthCertificate')}
-              activeValue={null}
-              control={control}
-              name='birthCertificate'
-            >
-              <FormBlock animated>
-                <QuizFieldTitle />
-                <FormImageInput />
-              </FormBlock>
-            </ConditionalFormWrapper>
-          </TranslationContextProvider>
-        )}
-      </QuizPage>
-    );
-  }
-);
+          </ConditionalFormWrapper>
+        </TranslationContextProvider>
+      )}
+    </QuizPage>
+  );
+});
